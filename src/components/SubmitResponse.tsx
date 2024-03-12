9f868cf1345dddd66844d9eceff5aff1cbc9ab80
Ashish Rao D S <ashishraods@SMGCC-MAC014.local>
2024-03-12 23:06:27 +0530

    fix: deployment issue

--- src/components/SubmitResponse.tsx
+++ src/components/SubmitResponse.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 import ThankYouScreen from './ThankYouScreen';
 
 interface Props {
